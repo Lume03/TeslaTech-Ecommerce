34a9b85f5f8eeb1a07e062bfea9f5f744f37e50d
Alonso luque <luquealonso151@gmail.com>
2025-11-22 22:07:15 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    // This is required to allow requests from the Firebase Studio dev environment.
+    allowedDevOrigins: [
+      '6000-firebase-studio-1749664212529.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev',
+      '9000-firebase-studio-1749664212529.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev',
+    ],
+  },
   images: {
     remotePatterns: [
       {
